6c376d34e1ff1276f6f42ae61b1733e8f72f1713
harshitsingh85420 <56180633+harshitsingh85420@users.noreply.github.com>
2025-08-28 15:14:37 +0530

    Update and rename next.config.ts to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: '**.supabase.co' },
       { protocol: 'https', hostname: 'images.unsplash.com' }
     ]
   }
 }
-
 export default nextConfig
